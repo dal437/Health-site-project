60c0ef0f7e27bf37acce86b50ee7b11ec5445a25
dalanao <dal437@nyu.edu>
2017-11-10 12:48:03 -0500

    Edited more details

--- src/working-project/app.js
+++ src/working-project/app.js
@@ -18,8 +18,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const medicalConditions = [{name: "Asthma",
+state: "TX",
+count: "45",
+description: "Difficulty breathing"}];
+
 app.get('/', (req, res) => {
-  res.render('layout.hbs');
+  let c = conditions;
+  const f = req.query.filterdisease;
+  if (f){
+    x = x.filter(condition => condition.description === f);
+  }
+	res.render('layout.hbs', {medicalConditions:x});
+});
+
+
+app.post('/', (req, res) => {
+	const medicalName = req.body.Name;
+  const medicalState = req.body.State;
+  const medicalCount = req.body.Count;
+  const medicalDescription = req.body.Description;
+  medicalConditions.push({name:medicalName, state:medicalState, count:medicalCount, description: medicalDescription});
+	res.redirect('/');
 });
 
 const PORT = process.env.PORT || 5000;
