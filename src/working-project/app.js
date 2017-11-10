bf3994cb837e09a1f83d596b164cee70208bb10a
dalanao <dal437@nyu.edu>
2017-11-10 12:51:16 -0500

    Reverted errors

--- src/working-project/app.js
+++ src/working-project/app.js
@@ -18,29 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const medicalConditions = [{name: "Asthma",
-state: "TX",
-count: "45",
-description: "Difficulty breathing"}];
-
 app.get('/', (req, res) => {
-  let c = conditions;
-  const f = req.query.filterdisease;
-  if (f){
-    x = x.filter(condition => condition.description === f);
-  }
-	res.render('layout.hbs', {medicalConditions:x});
+  res.render('layout.hbs');
 });
 
-
-app.post('/', (req, res) => {
-	const medicalName = req.body.Name;
-  const medicalState = req.body.State;
-  const medicalCount = req.body.Count;
-  const medicalDescription = req.body.Description;
-  medicalConditions.push({name:medicalName, state:medicalState, count:medicalCount, description: medicalDescription});
-	res.redirect('/');
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
