6d63325da9b11f7cf0e6c68e9bc17bf9afb0f294
dalanao <dal437@nyu.edu>
2017-11-10 17:26:05 -0500

    Still working

--- src/working-project/app.js
+++ src/working-project/app.js
@@ -1,4 +1,5 @@
 //app.js
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,48 +7,26 @@ const session = require('express-session');
 const path = require('path');
 const models = require('./db');
 
-// Server constants
+// enable sessions
 const sessionOptions = {
     secret: 'secretWord',
     saveUninitialized: false,
     resave: false,
 };
 
-// Configure appi
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up routes
 app.get('/', (req, res) => {
-    const d = req.query.director;
-    const q = {};
-    if (d) {
-        q.director = d;
-    }
-    models.Movie.find(q,
-        (err, movies) => res.render('layout.hbs', { movies }));
+  res.render('layout.hbs');
 });
 
-app.get('/working-project/add', (_, res) => res.render('add.hbs'));
-app.post('/working-project/add', (req, res) => {
-    const title = req.body.title;
-    const year = req.body.year;
-    const director = req.body.director;
-
-    const movie = new models.Movie({ title, year, director });
-    if (req.session.addedMovies) {
-        req.session.addedMovies.push(movie.toObject());
-    }
-    else {
-      req.session.addedMovies = [movie.toObject()];
-    }
-    movie.save(() => res.redirect('/'));
-});
-
-app.get('/working-project/about', (req, res) =>
-    res.render('about.hbs', { movies: req.session.addedMovies }));
+app.post('/', (req, res) => {
+  res.render('layout.hbs');
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- src/working-project/db.js
+++ src/working-project/db.js
@@ -1,19 +1,24 @@
-//db.js
+// 1ST DRAFT DATA MODEL
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/myapp';
 
+
 // my schema goes here!
-const MovieSchema = new Schema({
-    title: String,
-    year: Number,
-    director: String,
+const InfoSchema = new Schema({
+    age: Number,
+    gender: String,
+    state: String,
+    ethnicity: String,
 });
 
-const Movie = mongoose.model('Movie', MovieSchema);
+// list of sites for user to access
+const Listofsites = new mongoose.Schema({
+  sites: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+});
 
 mongoose.connect(MONGO_URI);
 
 module.exports = {
-    Movie,
+    Listofsites,
 };
