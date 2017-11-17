d8509cf5e36b1b7ef304862aca6c8bad3d1436d2
dalanao <dal437@nyu.edu>
2017-11-17 17:22:17 -0500

    Completing Milestone 3

--- src/working-project/app.js
+++ src/working-project/app.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
+//const bootstrap = require('bootstrap');
 const models = require('./db');
 
 // enable sessions
@@ -28,7 +29,30 @@ app.get("/about", (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('layout.hbs');
+  const s = req.query.state;
+  const q = {};
+  if (s) {
+    q.state = s;
+  }
+  models.Info.find(q,
+    (err, infos) => res.render('layout.hbs', { infos }));
+});
+
+app.get('/data', (_, res) => res.render('data.hbs', {layout:false}));
+app.post('/data', (req, res) => {
+    const age = req.body.age;
+    const gender = req.body.gender;
+    const state = req.body.state;
+    const ethnicity = req.body.ethnicity;
+
+    const infoData = new models.Info({ age, gender, state, ethnicity });
+    if (req.session.addedInfo) {
+        req.session.addedInfo.push(infoData.toObject());
+    }
+    else {
+      req.session.addedInfo = [infoData.toObject()];
+    }
+    infoData.save(() => res.redirect('/'));
 });
 
 const PORT = process.env.PORT || 5000;
